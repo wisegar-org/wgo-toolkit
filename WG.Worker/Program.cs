268e08687fc701da229343dd0c10cee17cbb0d74
Yariel Rodriguez <41107774+yarielre@users.noreply.github.com>
2023-07-31 10:11:33 +0200

    Update to run settings updtes only by -s arg

--- WG.Worker/Program.cs
+++ WG.Worker/Program.cs
@@ -1,28 +1,39 @@
 using Spectre.Console;
 using WGOvh.Services;
 
+	
 AnsiConsole.MarkupLine($"[underline springgreen3_1 bold]WGO WORKER[/]");
 Console.WriteLine();
+
 
 SettingsService.PrintSettings();
 Console.WriteLine();
 
-var clearSettings = AnsiConsole.Confirm("Do you want to clear the settings", false);
+AnsiConsole.WriteLine("To edit settings run command using option: -s");
+Console.WriteLine();
 
-if (clearSettings)
+if (args.Length > 0 && args[0] == "-s")
 {
-	await SettingsService.CleanSettingsAsync();
-	Console.WriteLine();
-	SettingsService.PrintSettings();
-	Console.WriteLine();
+	var clearSettings = AnsiConsole.Confirm("Do you want to clear the settings", false);
+
+	if (clearSettings)
+	{
+		await SettingsService.CleanSettingsAsync();
+		Console.WriteLine();
+		SettingsService.PrintSettings();
+		Console.WriteLine();
+	}
+
+	var updateSettings = AnsiConsole.Confirm("Do you want to update the settings", false);
+
+	if (updateSettings)
+	{
+		await SettingsService.GetInputSettingsAsync();
+		Console.WriteLine();
+	}
 }
 
-var updateSettings =  AnsiConsole.Confirm("Do you want to update the settings", false);
 
-if (updateSettings) {
-	await SettingsService.GetInputSettingsAsync();
-	Console.WriteLine();
-}
 
 IHost host = AppBuilder.Build(args)
 	.ConfigureServices((hostContext, services) =>
